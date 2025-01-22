f7aa10327acea1d396cc156abf7cf06398de89df
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-01-22 23:24:23 +0500

    complete signUp page Ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LoginPage } from "./routes/Routes.jsx";
 const App = () => {
   return (
-    <div >App</div>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage/>}/>
 
-export default App
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
