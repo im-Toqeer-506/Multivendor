a2c4391fffe1e7eb53243546bf2f56541d9e92d2
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-01-25 23:25:51 +0500

    error handler

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,62 +1,27 @@
-// const Errorhandler = require("../utils/ErrorHandler");
-// module.exports = (err, req, res, next) => {
-//   err.statusCode = err.statusCode || 500;
-//   err.message = err.message || "Internal Server Error!";
-//   //wrong mongoDB id error
-//   if (err.name === "CastError") {
-//     const message = `Resources not Found with this id .. Invalid ${err.path}`;
-//     err = new Errorhandler(message, 400);
-//   }
-//   //Duplicate key Error
-//   if (err.code === 11000) {
-//     const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`; 
-//     err = new Errorhandler(message, 409);
-//   }
-//   //wrong jwt error
-//   if (err.name === "JsonWebTokenError") {
-//     const message = `Your Url is invalid!,Please try again Later `;
-//     err = new Errorhandler(message, 401);
-//   }
-//   //jwt token expired
-//   if (err.name === "TokenExpiredError") {
-//     const message = `Your Url is Expired!Please try again Later `;
-//     err = new Errorhandler(message, 401);
-//   }
-//   res.status(err.statusCode).json({
-//     success: false,
-//     message: err.message,
-//   });
-// };
-const ErrorHandler = require("../utils/ErrorHandler");
-
+const Errorhandler = require("../utils/ErrorHandler");
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.message = err.message || "Internal server Error";
-
-  // wrong mongodb id error
+  err.message = err.message || "Internal Server Error!";
+  //wrong mongoDB id error
   if (err.name === "CastError") {
-    const message = `Resources not found with this id.. Invalid ${err.path}`;
-    err = new ErrorHandler(message, 400);
+    const message = `Resources not Found with this id .. Invalid ${err.path}`;
+    err = new Errorhandler(message, 400);
   }
-
-  // Duplicate key error
+  //Duplicate key Error
   if (err.code === 11000) {
-    const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`;
-    err = new ErrorHandler(message, 400);
+    const message = `Duplicate key ${Object.keys(err.keyValue)} Entered`; 
+    err = new Errorhandler(message, 409);
   }
-
-  // wrong jwt error
+  //wrong jwt error
   if (err.name === "JsonWebTokenError") {
-    const message = `Your url is invalid please try again letter`;
-    err = new ErrorHandler(message, 400);
+    const message = `Your Url is invalid!,Please try again Later `;
+    err = new Errorhandler(message, 401);
   }
-
-  // jwt expired
+  //jwt token expired
   if (err.name === "TokenExpiredError") {
-    const message = `Your Url is expired please try again letter!`;
-    err = new ErrorHandler(message, 400);
+    const message = `Your Url is Expired!Please try again Later `;
+    err = new Errorhandler(message, 401);
   }
-
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
