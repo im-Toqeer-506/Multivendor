5188d02794ae0e381e3b35c365e50931c8a92f9c
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-01-26 00:04:12 +0500

    persisting error

--- server/app.js
+++ server/app.js
@@ -23,15 +23,5 @@ const user = require("./controller/user");
 //Mount Routes
 app.use("/api/v2/user", user);
 //Error Handling Middleware
-// app.use(ErrorHandler);
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(ErrorHandler);
 module.exports = app;
-
