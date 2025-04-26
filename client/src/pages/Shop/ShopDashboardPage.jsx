33c75cc72dac1d8793a6ee25045644f81a4aab5c
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-04-26 13:20:00 +0500

    completing the shop dashboard

--- client/src/pages/Shop/ShopDashboardPage.jsx
+++ client/src/pages/Shop/ShopDashboardPage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
 import DashboardSidebar from "../../components/shop/Layout/ShopDashboardSidebar";
+import DashBoardHero from "../../components/Shop/DashBoardHero";
 const ShopDashboardPage = () => {
   return (
     <div>
       <DashboardHeader />
-      <div className="flex items-center justify-between w-full">
+      <div className="flex items-start justify-between w-full">
         <div className="800px:w-[330px] w-[80px]">
           <DashboardSidebar active={1} />
         </div>
+        <DashBoardHero />
       </div>
     </div>
   );
