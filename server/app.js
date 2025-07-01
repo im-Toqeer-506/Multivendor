b8862bed2911faa80d612929044c24412bc8aa89
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-07-01 09:45:18 +0500

    fixing the cors bug

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://client-eight-coral.vercel.app",
     credentials: true,
   })
 );
