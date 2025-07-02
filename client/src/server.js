5b8b2904d665c01680b775c653460a11ed0191a9
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-07-03 01:44:09 +0500

    resolving the cors issue

--- client/src/server.js
+++ client/src/server.js
@@ -1,2 +1,2 @@
-export const server = "backend-theta-weld-83.vercel.app/api/v2";
-export const backend_url = "backend-theta-weld-83.vercel.app";
+export const server = "https://backend-theta-weld-83.vercel.app/api/v2";
+export const backend_url = "https://backend-theta-weld-83.vercel.app";
