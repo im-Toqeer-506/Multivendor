86fc63a49b5a3692c4c28655bfd47b518c44dc12
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-07-03 01:14:52 +0500

    updated env for email verification

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -3,7 +3,7 @@ const sendMail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    service: process.env.SMPT_SERVICE,
+    service: process.env.SMTP_SERVICE,
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
